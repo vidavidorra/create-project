fc176f562174adbd8f6e2c595af7c9e9783915aa
Jeroen de Bruijn <vv9239@gmail.com>
2024-01-06 13:24:26 +0100

    ci: run pre-commit tests for package and tsconfig changes

--- .github/lint-staged.js
+++ .github/lint-staged.js
@@ -1,5 +1,6 @@
 const config = {
   '*.{ts,tsx,js,jsx}': ['xo --fix', () => 'ava'],
+  '(package*|tsconfig).json': [() => 'ava'],
   '*.{vue,css,less,scss,html,htm,json,md,markdown,yml,yaml}':
     'prettier --write',
 };
