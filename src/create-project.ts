1932f14843112943ae3a20c79e221906eca0cc27
Jeroen de Bruijn <vv9239@gmail.com>
2025-06-02 21:10:46 +0200

    fix: optimise writing the files, by writing them asynchronously

--- src/create-project.ts
+++ src/create-project.ts
@@ -2,13 +2,12 @@ import {type OptionsInput as Options, schema} from './options.js';
 import {files} from './content/index.js';
 
 async function createProject(options: Options): Promise<string[]> {
-  const paths: string[] = [];
-  for await (const file of files(schema.parse(options))) {
-    await file.write();
-    paths.push(file.path);
-  }
-
-  return paths;
+  return Promise.all(
+    files(schema.parse(options)).map(async (file) => {
+      await file.write();
+      return file.path;
+    }),
+  );
 }
 
 export {createProject};
